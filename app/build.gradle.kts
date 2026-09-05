b111e626a33996aa51e4e366e4a3313970ab3943
染指二进制 <shr006319@gmail.com>
2026-09-05 17:45:54 +0800

    chore(release): v1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.sirandev.phototimefixer"
         minSdk = 24
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
